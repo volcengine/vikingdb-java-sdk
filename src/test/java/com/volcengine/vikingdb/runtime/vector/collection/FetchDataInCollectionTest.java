86a45fe76e94ac5f95519c40f5723ac6a5265d3a
liuyang.max <liuyang.max@bytedance.com>
2025-09-26 15:31:44 +0800

    add open source header

--- src/test/java/com/volcengine/vikingdb/runtime/vector/collection/FetchDataInCollectionTest.java
+++ src/test/java/com/volcengine/vikingdb/runtime/vector/collection/FetchDataInCollectionTest.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+// SPDX-License-Identifier: Apache-2.0 
+
 package com.volcengine.vikingdb.runtime.vector.collection;
 
 import com.volcengine.vikingdb.runtime.core.*;

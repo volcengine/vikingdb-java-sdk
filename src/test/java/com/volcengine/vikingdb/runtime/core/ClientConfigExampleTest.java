b8437c0a6e072504fbff041e87fa27ed560b9467
liuyang.max <liuyang.max@bytedance.com>
2026-03-17 16:26:27 +0800

    optimize custom http config params

--- src/test/java/com/volcengine/vikingdb/runtime/core/ClientConfigExampleTest.java
+++ src/test/java/com/volcengine/vikingdb/runtime/core/ClientConfigExampleTest.java
@@ -27,7 +27,7 @@ public class ClientConfigExampleTest {
         // 1) 重试策略：这里演示指数退避（ExpBackoff），并指定重试参数
         RetryStrategy retryStrategy = new ExpBackoffStrategy(
                 3,    // 最大重试次数：失败后最多再尝试 3 次
-                1000, // 初始退避时间(ms)：第一次重试前先等待多久
+                500, // 初始退避时间(ms)：第一次重试前先等待多久
                 8000, // 最大退避时间(ms)：退避上限，避免等待时间无限增长
                 true  // 是否开启抖动(jitter)：加入随机扰动，缓解同一时间集中重试
         );
@@ -37,18 +37,21 @@ public class ClientConfigExampleTest {
                 60, TimeUnit.SECONDS // 连接存活时间(TTL)：连接在池中最多保留多久，避免长期复用导致异常
         );
         cm.setMaxTotal(200);          // 最大总连接数：整个连接池允许的最大并发连接数
+        cm.setDefaultMaxPerRoute(200);   // 单路由最大连接数，由于同一个client始终请求同一个host，必须与 MaxTotal 匹配
+        cm.setValidateAfterInactivity(10000); // 空闲 10s 后的连接在复用前进行可用性检查
 
         // 3) 设置 超时配置：
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(500) // 从连接池获取连接的超时(ms)：池满时最多等待多久
                 .setConnectTimeout(1000)          // 建连超时(ms)：TCP 三次握手等建连过程的最大等待时间
-                .setSocketTimeout(60000)           // 读超时(ms)：建立连接后等待响应数据的最大时间
+                .setSocketTimeout(10000)           // 读超时(ms)：建立连接后等待响应数据的最大时间
                 .build();
 
         // 4) 构建 HttpClient：
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(cm)                    // 使用连接池配置
                 .setDefaultRequestConfig(requestConfig)      // 使用超时配置
+                .evictIdleConnections(30, TimeUnit.SECONDS) // 开启后台线程,定期清理过期和空闲连接
                 .build();
 
         // 5) 把 重试策略 和 HttpClient 注入到 ClientConfig
